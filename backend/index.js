e1d86c769b98f82b0f754b3f4bf41fe7cd22c38f
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2025-10-08 00:09:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the homepage!");
 });
 
-app.listen(5001, () => {
-  console.log("Server Created Successfully");
-  Connection();
-});
+// Connect DB at the top level
+Connection();
+export default app;
