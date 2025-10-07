c76714e0b35bd8a88748ba62f78e18131565447e
SACHIN KOLI <132149727+Sachinkoligit@users.noreply.github.com>
2025-10-07 23:05:12 +0530

    Update axios.js

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://team-management-plum.vercel.app/api",
+  baseURL: "https://team-management-pied.vercel.app/api",
 });
 
 export default axiosInstance;
